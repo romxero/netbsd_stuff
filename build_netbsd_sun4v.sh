72325ef3572ebe6fb651825546c515b548c9d140
romxero <romxero@crea4.creacompute.lbl.gov>
2018-06-01 01:08:24 -0700

    added new scripts and build process

--- build_netbsd_sun4v.sh
+++ build_netbsd_sun4v.sh
@@ -1,13 +1,18 @@
-jobs=4
+jobs=16
+
+#building tools first
+
+/home/romxero/netbsd_stuff/scripts/buildTools.sh randy-sparc64-sun4v sparc64 kernel=$kernel $jobs
+
 
 kernel=GENERIC.DEBUG
-./scripts/build_netbsd.sh randy-sparc64-sun4v sparc64 kernel=$kernel $jobs
+/home/romxero/netbsd_stuff/scripts/build_netbsd.sh randy-sparc64-sun4v sparc64 kernel=$kernel $jobs
 
 kernel=GENERIC
-./scripts/build_netbsd.sh randy-sparc64-sun4v sparc64 kernel=$kernel $jobs
+/home/romxero/netbsd_stuff/scripts/build_netbsd.sh randy-sparc64-sun4v sparc64 kernel=$kernel $jobs
 
 kernel=INSTALL
-./scripts/build_netbsd.sh randy-sparc64-sun4v sparc64 kernel=$kernel $jobs
+/home/romxero/netbsd_stuff/scripts/build_netbsd.sh randy-sparc64-sun4v sparc64 kernel=$kernel $jobs
 
 #kernel=GENERIC_SUN4U
 #/home/palle/scripts/build_netbsd.sh palle-sparc64-sun4v sparc kernel=$kernel $jobs
